f3f881ac9d887566c5c672f9f592dde6d6b23e0e
melloware <mellowaredev@gmail.com>
2023-10-24 11:24:44 -0400

    Added docs

--- deployment/src/main/java/io/quarkiverse/antivirus/deployment/devui/AntivirusDevUIProcessor.java
+++ deployment/src/main/java/io/quarkiverse/antivirus/deployment/devui/AntivirusDevUIProcessor.java
@@ -3,6 +3,9 @@ package io.quarkiverse.antivirus.deployment.devui;
 import java.util.Map;
 import java.util.Optional;
 
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
+
 import io.quarkiverse.antivirus.deployment.ClamAVContainer;
 import io.quarkiverse.antivirus.deployment.ClamAVDevServicesConfigBuildItem;
 import io.quarkus.deployment.IsDevelopment;
@@ -24,11 +27,13 @@ public class AntivirusDevUIProcessor {
             Optional<ClamAVDevServicesConfigBuildItem> configProps,
             BuildProducer<FooterPageBuildItem> footerProducer) {
         if (configProps.isPresent()) {
+            Config runtimeConfig = ConfigProvider.getConfig();
             Map<String, String> config = configProps.get().getConfig();
             final CardPageBuildItem card = new CardPageBuildItem();
 
             // ClamAV
-            if (config.containsKey("clamav.tcp.port")) {
+            if (runtimeConfig.getOptionalValue("quarkus.antivirus.clamav.enabled",
+                    Boolean.class).orElse(Boolean.FALSE) && config.containsKey("clamav.tcp.port")) {
                 final ExternalPageBuilder versionPage = Page.externalPageBuilder("ClamAV Port")
                         .icon("font-awesome-solid:virus-slash")
                         .url("https://www.clamav.net/")
@@ -42,6 +47,16 @@ public class AntivirusDevUIProcessor {
                         .componentLink("qwc-antivirus-log.js");
 
                 footerProducer.produce(new FooterPageBuildItem(mailLogPageBuilder));
+            }
+
+            // VirusTotal
+            if (runtimeConfig.getOptionalValue("quarkus.antivirus.virustotal.enabled",
+                    Boolean.class).orElse(Boolean.FALSE)) {
+                final ExternalPageBuilder versionPage = Page.externalPageBuilder("VirusTotal")
+                        .icon("font-awesome-solid:virus-slash")
+                        .url("https://www.virustotal.com/")
+                        .staticLabel("v3");
+                card.addPage(versionPage);
             }
 
             card.setCustomCard("qwc-antivirus-card.js");
