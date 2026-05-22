a2090d5438c7290d4a84a61700ac3a7e3e7bb751
Melloware <mellowaredev@gmail.com>
2026-05-22 13:21:45 -0400

    Fix #92: New DevServices model (#106)

--- deployment/src/main/java/io/quarkiverse/antivirus/deployment/ClamAVDevServicesProcessor.java
+++ deployment/src/main/java/io/quarkiverse/antivirus/deployment/ClamAVDevServicesProcessor.java
@@ -1,37 +1,40 @@
 package io.quarkiverse.antivirus.deployment;
 
-import java.util.HashMap;
+import static io.quarkus.devservices.common.ConfigureUtil.configureSharedServiceLabel;
+import static io.quarkus.devservices.common.ContainerLocator.locateContainerWithLabels;
+
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.function.Supplier;
+import java.util.Map;
+import java.util.function.Function;
 
 import org.jboss.logging.Logger;
 
-import io.quarkus.bootstrap.classloading.QuarkusClassLoader;
-import io.quarkus.deployment.IsProduction;
-import io.quarkus.deployment.annotations.BuildProducer;
+import io.quarkus.deployment.IsDevServicesSupportedByLaunchMode;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.annotations.BuildSteps;
 import io.quarkus.deployment.builditem.DevServicesResultBuildItem;
 import io.quarkus.deployment.builditem.DevServicesSharedNetworkBuildItem;
 import io.quarkus.deployment.builditem.DockerStatusBuildItem;
 import io.quarkus.deployment.builditem.LaunchModeBuildItem;
-import io.quarkus.deployment.console.ConsoleInstalledBuildItem;
-import io.quarkus.deployment.console.StartupLogCompressor;
+import io.quarkus.deployment.builditem.Startable;
 import io.quarkus.deployment.dev.devservices.DevServicesConfig;
-import io.quarkus.deployment.logging.LoggingSetupBuildItem;
 import io.quarkus.devservices.common.ContainerAddress;
 import io.quarkus.devservices.common.ContainerLocator;
-import io.quarkus.devservices.common.ContainerShutdownCloseable;
+import io.quarkus.runtime.configuration.ConfigUtils;
 
 /**
  * Starts a ClamAV server as a dev service if needed.
  */
-@BuildSteps(onlyIfNot = IsProduction.class, onlyIf = DevServicesConfig.Enabled.class)
+@BuildSteps(onlyIf = { IsDevServicesSupportedByLaunchMode.class, DevServicesConfig.Enabled.class })
 public class ClamAVDevServicesProcessor {
 
     private static final Logger log = Logger.getLogger(ClamAVDevServicesProcessor.class);
+
+    private static final String FEATURE = "antivirus";
+
+    private static final String CLAMAV_HOST = "quarkus.antivirus.clamav.host";
+
+    private static final String CLAMAV_PORT = "quarkus.antivirus.clamav.port";
 
     /**
      * Label to add to shared Dev Service for ClamAV running in containers.
@@ -39,138 +42,100 @@ public class ClamAVDevServicesProcessor {
      */
     static final String DEV_SERVICE_LABEL = "quarkus-dev-service-clamav";
 
-    static final ContainerLocator containerLocator = new ContainerLocator(DEV_SERVICE_LABEL, ClamAVContainer.PORT_TCP);
-
-    static volatile DevServicesResultBuildItem.RunningDevService devService;
-    static volatile ClamAVBuildConfig cfg;
-    static volatile boolean first = true;
+    private static final ContainerLocator containerLocator = locateContainerWithLabels(ClamAVContainer.PORT_TCP,
+            DEV_SERVICE_LABEL);
 
     @BuildStep
     public DevServicesResultBuildItem startClamAVDevService(
             DockerStatusBuildItem dockerStatusBuildItem,
             LaunchModeBuildItem launchMode,
             ClamAVBuildConfig clamAVConfig,
-            Optional<ConsoleInstalledBuildItem> consoleInstalledBuildItem,
-            LoggingSetupBuildItem loggingSetupBuildItem,
             DevServicesConfig devServicesConfig,
-            BuildProducer<ClamAVDevServicesConfigBuildItem> clamAvBuildItemBuildProducer,
-            List<DevServicesSharedNetworkBuildItem> devServicesSharedNetworkBuildItem) {
-
-        if (devService != null) {
-            boolean shouldShutdownTheBroker = !ClamAVBuildConfig.isEqual(clamAVConfig, cfg);
-            if (!shouldShutdownTheBroker) {
-                return devService.toBuildItem();
-            }
-            shutdown();
-            cfg = null;
-        }
-
-        StartupLogCompressor compressor = new StartupLogCompressor(
-                (launchMode.isTest() ? "(test) " : "") + "ClamAV Dev Services Starting:",
-                consoleInstalledBuildItem, loggingSetupBuildItem);
-        try {
-            devService = startClamAV(dockerStatusBuildItem, clamAVConfig, devServicesConfig, launchMode,
-                    !devServicesSharedNetworkBuildItem.isEmpty());
-            if (devService == null) {
-                compressor.closeAndDumpCaptured();
-            } else {
-                compressor.close();
-            }
-        } catch (Throwable t) {
-            compressor.closeAndDumpCaptured();
-            throw new RuntimeException(t);
-        }
-
-        if (devService == null) {
+            List<DevServicesSharedNetworkBuildItem> sharedNetwork) {
+        if (devServiceDisabled(dockerStatusBuildItem, clamAVConfig)) {
             return null;
         }
 
-        if (devService.isOwner()) {
-            log.info("Dev Services for ClamAV started.");
-            clamAvBuildItemBuildProducer.produce(new ClamAVDevServicesConfigBuildItem(devService.getConfig()));
-        }
+        boolean useSharedNetwork = DevServicesSharedNetworkBuildItem.isSharedNetworkRequired(devServicesConfig,
+                sharedNetwork);
 
-        // Configure the watch dog
-        if (first) {
-            first = false;
-            Runnable closeTask = () -> {
-                if (devService != null) {
-                    shutdown();
-
-                    log.info("Dev Services for ClamAV shut down.");
-                }
-                first = true;
-                devService = null;
-                cfg = null;
-            };
-            QuarkusClassLoader cl = (QuarkusClassLoader) Thread.currentThread().getContextClassLoader();
-            ((QuarkusClassLoader) cl.parent()).addCloseTask(closeTask);
-        }
-        cfg = clamAVConfig;
-        return devService.toBuildItem();
+        return containerLocator
+                .locateContainer(clamAVConfig.serviceName(), clamAVConfig.shared(), launchMode.getLaunchMode())
+                .map(containerAddress -> DevServicesResultBuildItem.discovered()
+                        .name(FEATURE)
+                        .containerId(containerAddress.getId())
+                        .config(discoveredConfig(containerAddress, clamAVConfig))
+                        .build())
+                .orElseGet(() -> DevServicesResultBuildItem.owned()
+                        .feature(FEATURE)
+                        .serviceName(clamAVConfig.serviceName())
+                        .serviceConfig(clamAVConfig)
+                        .startable(() -> createContainer(clamAVConfig, devServicesConfig, useSharedNetwork, launchMode))
+                        .configProvider(configProvider(clamAVConfig))
+                        .postStartHook(ClamAVDevServicesProcessor::logStarted)
+                        .build());
     }
 
-    private DevServicesResultBuildItem.RunningDevService startClamAV(DockerStatusBuildItem dockerStatusBuildItem,
-            ClamAVBuildConfig clamAVConfig,
-            DevServicesConfig devServicesConfig,
-            LaunchModeBuildItem launchMode,
-            boolean useSharedNetwork) {
-        if (!clamAVConfig.enabled()) {
-            // explicitly disabled
+    private static ClamAVContainer createContainer(ClamAVBuildConfig config, DevServicesConfig devServicesConfig,
+            boolean useSharedNetwork, LaunchModeBuildItem launchMode) {
+        ClamAVContainer container = new ClamAVContainer(config, useSharedNetwork);
+        devServicesConfig.timeout().ifPresent(container::withStartupTimeout);
+        configureSharedServiceLabel(container, launchMode.getLaunchMode(), DEV_SERVICE_LABEL, config.serviceName());
+        return container;
+    }
+
+    private static Map<String, Function<ClamAVContainer, String>> configProvider(ClamAVBuildConfig config) {
+        String prefix = config.serviceName();
+        return Map.of(
+                CLAMAV_HOST, ClamAVContainer::getEffectiveHost,
+                CLAMAV_PORT, container -> Integer.toString(container.getEffectivePort()),
+                prefix + ".tcp.host", ClamAVContainer::getEffectiveHost,
+                prefix + ".tcp.port", container -> Integer.toString(container.getEffectivePort()));
+    }
+
+    private static Map<String, String> discoveredConfig(ContainerAddress address, ClamAVBuildConfig config) {
+        String prefix = config.serviceName();
+        return Map.of(
+                CLAMAV_HOST, address.getHost(),
+                CLAMAV_PORT, Integer.toString(address.getPort()),
+                prefix + ".tcp.host", address.getHost(),
+                prefix + ".tcp.port", Integer.toString(address.getPort()));
+    }
+
+    private static void logStarted(Startable startable) {
+        log.infof(
+                "Dev Services for ClamAV started. Other Quarkus applications in dev mode will find the "
+                        + "server automatically. For Quarkus applications in production mode, connect using "
+                        + "-D%s=%s -D%s=%s",
+                CLAMAV_HOST, asClamAVContainer(startable).getEffectiveHost(),
+                CLAMAV_PORT, asClamAVContainer(startable).getEffectivePort());
+    }
+
+    private static ClamAVContainer asClamAVContainer(Startable startable) {
+        return (ClamAVContainer) startable;
+    }
+
+    private boolean devServiceDisabled(DockerStatusBuildItem dockerStatusBuildItem, ClamAVBuildConfig config) {
+        if (!config.enabled()) {
             log.warn("Not starting dev services for ClamAV, as it has been disabled in the config.");
-            return null;
+            return true;
         }
 
-        if (!Objects.equals(ClamAVContainer.getPort(), ClamAVContainer.PORT_TCP)) {
-            // no mailer configured
+        if (ConfigUtils.isPropertyNonEmpty(CLAMAV_PORT)) {
             log.warn("Not starting dev services for ClamAV, as 'quarkus.antivirus.clamav.port' has been configured.");
-            return null;
+            return true;
         }
 
-        if (ClamAVContainer.getTcpHost().isPresent()) {
-            // no mailer configured
+        if (ConfigUtils.isPropertyNonEmpty(CLAMAV_HOST)) {
             log.warn("Not starting dev services for ClamAV, as 'quarkus.antivirus.clamav.host' has been configured.");
-            return null;
+            return true;
         }
 
         if (!dockerStatusBuildItem.isContainerRuntimeAvailable()) {
             log.warn("Docker isn't working, not starting dev services for ClamAV.");
-            return null;
+            return true;
         }
 
-        final Optional<ContainerAddress> maybeContainerAddress = containerLocator.locateContainer(clamAVConfig.serviceName(),
-                useSharedNetwork,
-                launchMode.getLaunchMode());
-
-        final Supplier<DevServicesResultBuildItem.RunningDevService> defaultClamAvSupplier = () -> {
-            final ClamAVContainer container = new ClamAVContainer(clamAVConfig, useSharedNetwork);
-            devServicesConfig.timeout().ifPresent(container::withStartupTimeout);
-            container.start();
-
-            return new DevServicesResultBuildItem.RunningDevService(clamAVConfig.serviceName(),
-                    container.getContainerId(),
-                    new ContainerShutdownCloseable(container, clamAVConfig.serviceName()),
-                    container.getExposedConfig());
-        };
-
-        return maybeContainerAddress
-                .map(containerAddress -> new DevServicesResultBuildItem.RunningDevService(clamAVConfig.serviceName(),
-                        containerAddress.getId(),
-                        null,
-                        new HashMap<>()))
-                .orElseGet(defaultClamAvSupplier);
-    }
-
-    private void shutdown() {
-        if (devService != null) {
-            try {
-                log.info("Dev Services for ClamAV shutting down...");
-                devService.close();
-            } catch (Throwable e) {
-                log.error("Failed to stop the ClamAV server", e);
-            } finally {
-                devService = null;
-            }
-        }
+        return false;
     }
 }
